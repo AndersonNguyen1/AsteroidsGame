fb61ae9fefd5a35d449ac0f423962605e8a28afe
Anderson Nguyen <anguyen135@gmail.com>
2014-11-30 19:56:02 -0800

    Asteroids

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip a = new SpaceShip();
 Stars[] stars = new Stars[40];
 
@@ -62,9 +78,9 @@ class SpaceShip extends Floater
 public void keyPressed()
 {
   if(keyCode=='W')
-    a.accelerate(.5);
+    a.accelerate(.5f);
   if(keyCode=='S')
-    a.accelerate(-.5);
+    a.accelerate(-.5f);
   if(keyCode=='D')
     a.rotate(5);
   if(keyCode=='A')
@@ -178,3 +194,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
